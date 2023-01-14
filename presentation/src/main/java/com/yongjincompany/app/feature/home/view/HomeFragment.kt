825d004f03df51eca7d07fa7de29e337246cfab3
leesin <dragon02229@naver.com>
2023-01-15 07:50:31 +0900

    ⚡ :: gogo doge Coin!

--- presentation/src/main/java/com/yongjincompany/app/feature/home/view/HomeFragment.kt
+++ presentation/src/main/java/com/yongjincompany/app/feature/home/view/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.yongjincompany.app.feature.home.view
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yongjincompany.app.R
 import com.yongjincompany.app.databinding.FragmentHomeBinding
+import com.yongjincompany.app.feature.MainActivity
+import com.yongjincompany.app.feature.question.view.FinanceFragment
 import com.yongjincompany.app.util.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(
@@ -17,6 +20,22 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
         savedInstanceState: Bundle?
     ): View? {
 
+
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun initView() {
+        super.initView()
+
+        binding.ivFinance.setOnClickListener {
+            val mainActivity = activity as MainActivity
+
+
+            mainActivity.supportFragmentManager.beginTransaction()
+                .replace(R.id.mainContainer, FinanceFragment())
+                .addToBackStack("d")
+                .commit()
+        }
+
     }
 }
