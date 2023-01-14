27524b7a440b6afd17334dd44c9a8aea8056274c
leesin <dragon02229@naver.com>
2023-01-15 07:38:06 +0900

    ⚡ :: Implement active btn Logic

--- presentation/src/main/java/com/yongjincompany/app/feature/question/view/QuestionFragment.kt
+++ presentation/src/main/java/com/yongjincompany/app/feature/question/view/QuestionFragment.kt
@@ -1,5 +1,6 @@
 package com.yongjincompany.app.feature.question.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -32,27 +33,48 @@ class QuestionFragment : BaseFragment<FragmentQuestionBinding>(
             .commit()
 
         binding.btnSocialLife.setOnClickListener {
+            binding.btnSocialLife.setBackgroundResource(R.drawable.question_top_button_background_active)
+            binding.btnLaw.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnEtc.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnFinance.setBackgroundResource(R.drawable.question_top_button_background)
             mainActivity.supportFragmentManager.beginTransaction()
                 .replace(R.id.questionContainer, SocialLifeFragment())
                 .commit()
         }
 
         binding.btnFinance.setOnClickListener {
+            binding.btnSocialLife.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnLaw.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnEtc.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnFinance.setBackgroundResource(R.drawable.question_top_button_background_active)
             mainActivity.supportFragmentManager.beginTransaction()
                 .replace(R.id.questionContainer, FinanceFragment())
                 .commit()
         }
 
         binding.btnEtc.setOnClickListener {
+            binding.btnSocialLife.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnLaw.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnEtc.setBackgroundResource(R.drawable.question_top_button_background_active)
+            binding.btnFinance.setBackgroundResource(R.drawable.question_top_button_background)
             mainActivity.supportFragmentManager.beginTransaction()
                 .replace(R.id.questionContainer, ETCFragment())
                 .commit()
         }
 
         binding.btnLaw.setOnClickListener {
+            binding.btnSocialLife.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnLaw.setBackgroundResource(R.drawable.question_top_button_background_active)
+            binding.btnEtc.setBackgroundResource(R.drawable.question_top_button_background)
+            binding.btnFinance.setBackgroundResource(R.drawable.question_top_button_background)
             mainActivity.supportFragmentManager.beginTransaction()
                 .replace(R.id.questionContainer, LawFragment())
                 .commit()
+        }
+
+        binding.btnMakeQuestion.setOnClickListener {
+            val intent = Intent(context, PostQuestionActivity::class.java)
+            startActivity(intent)
         }
     }
 }
