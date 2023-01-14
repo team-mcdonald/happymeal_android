b15d4fa4fe300d9556c4d43768f6f81aadefa40c
leesin <dragon02229@naver.com>
2023-01-15 04:07:12 +0900

    ⚡ :: Implment Login

--- presentation/src/main/java/com/yongjincompany/app/feature/auth/login/view/LoginActivity.kt
+++ presentation/src/main/java/com/yongjincompany/app/feature/auth/login/view/LoginActivity.kt
@@ -2,18 +2,41 @@ package com.yongjincompany.app.feature.auth.login.view
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.yongjincompany.app.R
 import com.yongjincompany.app.databinding.ActivityLoginBinding
 import com.yongjincompany.app.feature.MainActivity
+import com.yongjincompany.app.feature.auth.login.viewmodel.LoginViewModel
 import com.yongjincompany.app.util.BaseActivity
+import com.yongjincompany.app.util.repeatOnStarted
+import com.yongjincompany.domain.param.LoginParam
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(
     R.layout.activity_login
 ) {
+    private val vm: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
+        repeatOnStarted {
+            vm.loginEvent.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event: LoginViewModel.LoginEvent) {
+        when (event) {
+            is LoginViewModel.LoginEvent.SuccessLogin -> {
+                val intent = Intent(baseContext, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            is LoginViewModel.LoginEvent.ErrorMessage -> showShortToast(event.errorMessage)
+
+        }
     }
 
     override fun initView() {
@@ -22,10 +45,13 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(
         binding.ivBack.setOnClickListener {
             finish()
         }
-
         binding.btnLogin.setOnClickListener {
-            val intent = Intent(baseContext, MainActivity::class.java)
-            startActivity(intent)
+            vm.login(
+                LoginParam(
+                    binding.etId.text.toString(),
+                    binding.etPassword.text.toString()
+                )
+            )
         }
     }
 }
